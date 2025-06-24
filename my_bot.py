d0ac7b2b015880f97f1400699430be673ccf4931
Kira92377 <bekkerks.1@gmail.com>
2025-06-24 15:50:20 +0300

    Добавлены команды /pass и /help

--- my_bot.py
+++ my_bot.py
@@ -1,4 +1,5 @@
 import telebot
+from bot_logic import gen_pass
 
 # Замени 'TOKEN' на токен твоего бота
 # Этот токен ты получаешь от BotFather, чтобы бот мог работать
@@ -15,6 +16,15 @@ def send_hello(message):
 @bot.message_handler(commands=['bye'])
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=["pass"])
+def send_pass(message):
+    password = gen_pass(10)
+    bot.reply_to(message, f"Вот твой пароль: {password}")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "Я могу выполнить следующие команды: \n /start - Начать работу\n /help - Показать это сообщение\n /hello - Поприветствовать пользователя\n /bye - Попрощаться с пользователем\n /pass - Сгенерировать пароль")
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
